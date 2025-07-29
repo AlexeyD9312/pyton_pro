860888969b35ea04885afe13abf48ab4d4bfdb15
Alex <aviatoaviator171513@gmail.com>
2025-07-29 14:09:18 +0000

    custom admin

--- psql_real_estate/objects/admin.py
+++ psql_real_estate/objects/admin.py
@@ -2,6 +2,29 @@ from django.contrib import admin
 from .models import District
 
 
-admin.site.register(District)
+@admin.register(District)
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ['district_ID','city_ID','district_name','is_admin_district']
+    list_filter = ['city_ID', 'is_admin_district']
+    search_fields = ['district_name']
+    ordering = ['district_name']
+    list_per_page = 15
+    list_editable = ['is_admin_district',]
+
+    fieldsets = (
+        ('General information',{
+            'fields' : ['district_ID','district_name']
+        }),
+        ('Images', {
+            'fields': ['image'],
+            'description' : 'download images'
+        })
+    )
+
+    def clean(self):
+        super().clean()
+        if len(self.district_name) > 100:
+            raise ValidationError('Use more short name')
+
 
 
--- psql_real_estate/objects/models.py
+++ psql_real_estate/objects/models.py
@@ -8,12 +8,17 @@ class District(models.Model):
     district_ID = models.AutoField(primary_key = True)
     city_ID = models.IntegerField(null = False)
     district_name = models.CharField(max_length = 100)
+    is_admin_district = models.BooleanField(default = False)
+    images = models.ImageField(upload_to = 'district_images/', null = True, blank = True)
+
 
     class Meta:
         permissions = [
             ('Can_administrate_lib', 'User can administrate the library'),
             ('Can_add_object', 'User can administrate DB')
         ]
+
+    ordering = ['district_name']
 
     def __str__(self):
         return self.district_name
